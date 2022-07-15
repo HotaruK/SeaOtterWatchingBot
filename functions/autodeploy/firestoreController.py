bf1aa2c1f9a64b5031ff0f1aa526994d6bca4e48
Hotaru Komajo <30549239+HotaruK@users.noreply.github.com>
2022-07-15 14:35:38 -0700

    update main

--- functions/autodeploy/firestoreController.py
+++ functions/autodeploy/firestoreController.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 
-def get_connection(project_id):
+def get_firestore_connection(project_id):
     # Use the application default credentials
     cred = credentials.ApplicationDefault()
     firebase_admin.initialize_app(cred, {
--- functions/autodeploy/main.py
+++ functions/autodeploy/main.py
@@ -1,28 +1,50 @@
 import functions_framework
 from datetime import datetime, timedelta
-from .settings import firestore_collection_name, firestore_project_id, montereybay_sea_otter_url
-from .firestoreController.firestore import get_connection
+from .settings import firestore_collection_name, firestore_project_id, montereybay_sea_otter_url, \
+    twitter_consumer_key, twitter_consumer_secret, twitter_token, twitter_token_secret
+from .firestoreController import get_firestore_connection
+from .twitterController import get_twitter_connection, post_tweet
+
+s = [
+    firestore_project_id, firestore_collection_name,
+    twitter_token, twitter_token_secret, twitter_consumer_key, twitter_consumer_secret
+]
 
 
 @functions_framework.cloud_event
 def main_cloud_event(cloud_event):
     print(f"Received event with ID: {cloud_event['id']} and data {cloud_event.data}")
+    if not _check_secrets():
+        print(s)
+        raise RuntimeError("secrets have None, please check it")
 
     current_ev_time, next_ev_time = _get_base_time_events(datetime.now())
-    db = get_connection(firestore_project_id)
+    print(f"target time: current->{current_ev_time}, next->{next_ev_time}")
+
+    db = get_firestore_connection(firestore_project_id)
+    prep_tweets = []
 
     # current start event
     current_events = _get_events_by_time(firestore_collection_name, current_ev_time, db)
     for ev in current_events:
         url = montereybay_sea_otter_url if ev.location == "Sea Otters exhibit" else ""
         text = f"{ev.name} has just begun! {url}"
-    # todo:tweet
+        prep_tweets.append(text)
 
     # next start event
     next_events = _get_events_by_time(firestore_collection_name, next_ev_time, db)
     for ev in next_events:
         text = f"{ev.name} will begin soon, starting at {ev.date}"
-    # todo: tweet
+        prep_tweets.append(text)
+
+    print(f"current->{len(current_events)}items, next->{len(next_events)}items")
+
+    # tweet
+    if prep_tweets:
+        print(prep_tweets)
+        t = get_twitter_connection(twitter_token, twitter_token_secret,
+                                   twitter_consumer_key, twitter_consumer_secret)
+        post_tweet(text, t)
 
 
 def _get_events_by_time(collection: str, time: str, db) -> list:
@@ -39,4 +61,5 @@ def _get_base_time_events(now: datetime) -> tuple[str, str]:
     return adjust_now.strftime(fmt), next_event_time.strftime(fmt)
 
 
-# def _post_notificaton_tweet():
+def _check_secrets():
+    return False if None in s else True
