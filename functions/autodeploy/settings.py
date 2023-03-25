5cc2e801a0b1e2ef553cb954c4cb5524e8bcc56a
Hotaru Komajo <30549239+HotaruK@users.noreply.github.com>
2023-03-25 10:07:07 -0700

    Update settings.py

--- functions/autodeploy/settings.py
+++ functions/autodeploy/settings.py
@@ -6,7 +6,7 @@ import os
 firestore_project_id = os.getenv("FIRESTORE_PROJECT_ID")
 firestore_collection_name = os.getenv("FIRESTORE_COLLECTION_NAME")
 
-montereybay_sea_otter_url = "https://www.youtube.com/watch?v=JQnxefImhu8"
+montereybay_sea_otter_url = os.getenv("YOUTUBE_URL")
 
 # ----------------
 # Twitter
